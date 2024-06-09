8bb4e131fd5d7ba4b71ad9c2578a575c0cb4160c
DannyLien <danny.lien1109@gmail.com>
2024-06-09 16:13:18 +0800

    6-2, RecyelerView, 使用集合, 樣板使用 FindViewById

--- app/src/main/java/com/tom/shop202406/MainActivity.kt
+++ app/src/main/java/com/tom/shop202406/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tom.shop202406
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -12,14 +13,21 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.tom.shop202406.databinding.ActivityMainBinding
+import kotlin.collections.listOf
+import kotlin.collections.listOf as listOf1
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +36,26 @@ class MainActivity : AppCompatActivity() {
     private val RC_NICKNAME: Int = 210
     private val RC_SIGNUP: Int = 200
     val auth = FirebaseAuth.getInstance()
-
+    val functions = listOf<String>(
+        "Camera",
+        "Invite friend",
+        "Parking",
+        "Download coupons",
+        "News",
+        "Maps",
+        "Camera",
+        "Invite friend",
+        "Parking",
+        "Download coupons",
+        "News",
+        "Maps",
+        "Camera",
+        "Invite friend",
+        "Parking",
+        "Download coupons",
+        "News",
+        "Maps"
+    )
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,13 +80,10 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, colors)
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
         binding.contentMain.spinner.adapter = adapter
-        binding.contentMain.spinner.onItemSelectedListener = 
-            object : AdapterView.OnItemSelectedListener{
+        binding.contentMain.spinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
+                    parent: AdapterView<*>?, view: View?, position: Int, id: Long
                 ) {
                     Log.d(TAG, "onItemSelected: ${colors[position]}")
                 }
@@ -68,16 +92,42 @@ class MainActivity : AppCompatActivity() {
                     TODO("Not yet implemented")
                 }
             }
+
+        //RecyclerView
+        // functions 設定屬性
+        binding.contentMain.recycler.layoutManager = LinearLayoutManager(this)
+        binding.contentMain.recycler.setHasFixedSize(true)
+        binding.contentMain.recycler.adapter = FunctionAdapter()
+
+    }
+
+    inner class FunctionAdapter() : RecyclerView.Adapter<FunctionHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FunctionHolder {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.row_function, parent, false)
+            val holder = FunctionHolder(view)
+            return holder
+        }
+
+        override fun getItemCount(): Int {
+            return functions.size
+        }
+
+        override fun onBindViewHolder(holder: FunctionHolder, position: Int) {
+            holder.nameText.text = functions.get(position)
+        }
+
+    }
+
+    class FunctionHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var nameText: TextView = view.findViewById(R.id.name)
     }
 
     override fun onResume() {
         super.onResume()
 //        binding.contentMain.nickname.text = getNickname()
-        FirebaseDatabase.getInstance()
-            .getReference("users")
-            .child(auth.currentUser!!.uid)
-            .child("nickname")
-            .addListenerForSingleValueEvent(object : ValueEventListener {
+        FirebaseDatabase.getInstance().getReference("users").child(auth.currentUser!!.uid)
+            .child("nickname").addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
                     binding.contentMain.nickname.text = dataSnapshot.value as String
                 }
